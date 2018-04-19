9525c8b0c3964f796ef83729a8f396051b17d0ef
MirekLis <lismiroslaw@o2.pl>
2018-04-19 23:56:58 +0200

    Strona startowa aplikacji + przekierowania na formularze grup

---
+++ PROJEKTapp/PROJEKTapp/Forms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJEKTapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogistyka_Click(object sender, EventArgs e)
+        {
+            FormLogistyka Logistyka = new FormLogistyka();
+            Logistyka.Show();
+        }
+
+        private void btnKadry_Click(object sender, EventArgs e)
+        {
+            FormKadry Kadry = new FormKadry();
+            Kadry.Show();
+        }
+
+        private void btnProdukcja_Click(object sender, EventArgs e)
+        {
+            FormProdukcja Produkcja = new FormProdukcja();
+            Produkcja.Show();
+        }
+    }
+}
