c78ab170f2be82454c114ee2fc0739e0c69bd80d
MirekLis <lismiroslaw@o2.pl>
2018-04-20 00:50:29 +0200

    wstepny projekt formularza Kadr

--- PROJEKTapp/PROJEKTapp/Forms/FormKadry.Designer.cs
+++ PROJEKTapp/PROJEKTapp/Forms/FormKadry.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormKadry));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnPracownicy = new System.Windows.Forms.Button();
+            this.btnWynagrodzenia = new System.Windows.Forms.Button();
+            this.btnUrlopy = new System.Windows.Forms.Button();
+            this.btnSzkolenia = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(932, 46);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.Controls.Add(this.btnSzkolenia);
+            this.panel2.Controls.Add(this.btnUrlopy);
+            this.panel2.Controls.Add(this.btnWynagrodzenia);
+            this.panel2.Controls.Add(this.btnPracownicy);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 441);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(932, 135);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnPracownicy
+            // 
+            this.btnPracownicy.FlatAppearance.BorderSize = 0;
+            this.btnPracownicy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPracownicy.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPracownicy.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnPracownicy.Image = ((System.Drawing.Image)(resources.GetObject("btnPracownicy.Image")));
+            this.btnPracownicy.Location = new System.Drawing.Point(3, 0);
+            this.btnPracownicy.Name = "btnPracownicy";
+            this.btnPracownicy.Size = new System.Drawing.Size(233, 135);
+            this.btnPracownicy.TabIndex = 2;
+            this.btnPracownicy.Text = "Pracownicy";
+            this.btnPracownicy.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnPracownicy.UseVisualStyleBackColor = true;
+            this.btnPracownicy.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnWynagrodzenia
+            // 
+            this.btnWynagrodzenia.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnWynagrodzenia.FlatAppearance.BorderSize = 0;
+            this.btnWynagrodzenia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWynagrodzenia.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnWynagrodzenia.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnWynagrodzenia.Image = ((System.Drawing.Image)(resources.GetObject("btnWynagrodzenia.Image")));
+            this.btnWynagrodzenia.Location = new System.Drawing.Point(233, 0);
+            this.btnWynagrodzenia.Name = "btnWynagrodzenia";
+            this.btnWynagrodzenia.Size = new System.Drawing.Size(231, 135);
+            this.btnWynagrodzenia.TabIndex = 2;
+            this.btnWynagrodzenia.Text = "Wynagrodzenia";
+            this.btnWynagrodzenia.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnWynagrodzenia.UseVisualStyleBackColor = false;
+            this.btnWynagrodzenia.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUrlopy
+            // 
+            this.btnUrlopy.FlatAppearance.BorderSize = 0;
+            this.btnUrlopy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUrlopy.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnUrlopy.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnUrlopy.Image = ((System.Drawing.Image)(resources.GetObject("btnUrlopy.Image")));
+            this.btnUrlopy.Location = new System.Drawing.Point(466, 0);
+            this.btnUrlopy.Name = "btnUrlopy";
+            this.btnUrlopy.Size = new System.Drawing.Size(239, 135);
+            this.btnUrlopy.TabIndex = 2;
+            this.btnUrlopy.Text = "Urlopy";
+            this.btnUrlopy.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnUrlopy.UseVisualStyleBackColor = true;
+            this.btnUrlopy.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSzkolenia
+            // 
+            this.btnSzkolenia.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSzkolenia.FlatAppearance.BorderSize = 0;
+            this.btnSzkolenia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSzkolenia.Font = new System.Drawing.Font("Lucida Sans Unicode", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSzkolenia.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSzkolenia.Image = ((System.Drawing.Image)(resources.GetObject("btnSzkolenia.Image")));
+            this.btnSzkolenia.Location = new System.Drawing.Point(703, 0);
+            this.btnSzkolenia.Name = "btnSzkolenia";
+            this.btnSzkolenia.Size = new System.Drawing.Size(229, 135);
+            this.btnSzkolenia.TabIndex = 2;
+            this.btnSzkolenia.Text = "Szkolenia";
+            this.btnSzkolenia.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSzkolenia.UseVisualStyleBackColor = false;
+            this.btnSzkolenia.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kadry i Finanse";
+            // 
+            // FormKadry
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(932, 576);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FormKadry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormKadry";
+            this.Load += new System.EventHandler(this.FormKadry_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnPracownicy;
+        private System.Windows.Forms.Button btnSzkolenia;
+        private System.Windows.Forms.Button btnUrlopy;
+        private System.Windows.Forms.Button btnWynagrodzenia;
+        private System.Windows.Forms.Label label1;
     }
 }
--- PROJEKTapp/PROJEKTapp/Forms/FormKadry.cs
+++ PROJEKTapp/PROJEKTapp/Forms/FormKadry.cs
@@ -16,5 +16,15 @@ namespace PROJEKTapp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormKadry_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
