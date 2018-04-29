f3c4d660b11c873a371ebbab5d1e6dd9aa1e7253
DESKTOP-6P2R3E2\Damian <d.szczesniak.wat@gmail.com>
2018-04-29 21:56:49 +0200

    Poprawa modelu bazy. Usuniecie wywyoływania widoków przy tworzeniu. Dodano wstępny mechanizm logowania. Dodano plik .txt z przykladowymi kontami do repozytorium.

--- PROJEKTapp/PROJEKTapp/Program.cs
+++ PROJEKTapp/PROJEKTapp/Program.cs
@@ -16,7 +16,7 @@ namespace PROJEKTapp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormLogowanie());
         }
     }
 }
