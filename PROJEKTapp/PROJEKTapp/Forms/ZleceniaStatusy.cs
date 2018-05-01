be9d291f5b876f7a2545f95c7c539dcaaf2e1e46
DESKTOP-6P2R3E2\Damian <d.szczesniak.wat@gmail.com>
2018-05-01 22:15:25 +0200

    Widoczność statusów w zależności od uprawnień.

--- PROJEKTapp/PROJEKTapp/Forms/ZleceniaStatusy.cs
+++ PROJEKTapp/PROJEKTapp/Forms/ZleceniaStatusy.cs
@@ -25,7 +25,23 @@ namespace PROJEKTapp
 
         private void ZleceniaStatusy_Load(object sender, EventArgs e)
         {
-            StatusZleceniaDGV.DataSource = db.AKTUALNY_STATUS_ZLECEN_NAZWY.ToList();
+            // StatusZleceniaDGV.DataSource = db.AKTUALNY_STATUS_ZLECEN_NAZWY.ToList();
+            switch (uprawnienia)
+            {
+                case 1:
+                    StatusZleceniaDGV.DataSource = db.AKTUALNY_STATUS_ZLECEN_NAZWY.ToList();
+                    break;
+                case 2:
+                    StatusZleceniaDGV.DataSource = db.AKTUALNY_STATUS_ZLECEN_NAZWY.Where(a => a.Status.Value < 5 || a.Status.Value > 5 && a.Status.Value < 9).ToList();
+                    break;
+                case 3:
+                    StatusZleceniaDGV.DataSource = db.AKTUALNY_STATUS_ZLECEN_NAZWY.Where(a => a.Status.Value < 7 && a.Status.Value > 3 ).ToList();
+                    break;
+                case 4:
+                    StatusZleceniaDGV.DataSource = db.AKTUALNY_STATUS_ZLECEN_NAZWY.Where(a => a.Status.Value > 7).ToList();
+                    break;
+
+            }
         }
     }
 }
