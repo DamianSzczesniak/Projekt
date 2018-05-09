9052dc66685d2cbc740a26f5afad7dad47770a32
Kowalczyk Oliwia <oliwiakowalczyk@vp.pl>
2018-05-09 12:18:41 +0200

    poprawienie wyświewtlania szczegółów pracownika

--- PROJEKTapp/PROJEKTapp/Forms/FormKadry.cs
+++ PROJEKTapp/PROJEKTapp/Forms/FormKadry.cs
@@ -329,6 +329,7 @@ namespace PROJEKTapp
                 ListaPracownikow.DataSource = db.PRACOWNICY_ZATRUDNIENI.ToList();
                 ListaPracownikow.Refresh();
                 czyscform();
+                pnlUserField.Hide();
             }
         }
 
