931ed6e6489aecf1233a8af29292fc8eb29bbbfb
test <kaliszmat1@gmail.com>
2018-05-10 08:21:42 +0200

    update ewidencja

---
+++ PROJEKTapp/PROJEKTapp/Forms_Produkcja/FormNowaMaszyna.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJEKTapp.Forms_Produkcja
+{
+    public partial class FormNowaMaszyna : Form
+    {
+        KWZP_PROJEKTEntities db;
+        public FormNowaMaszyna(KWZP_PROJEKTEntities db)
+        {
+            this.db = db;
+            InitializeComponent();
+        }
+
+        private void DodajMaszyne_Click(object sender, EventArgs e)
+        {
+            MODELE_MASZYN noweModele_Maszyn = new MODELE_MASZYN();
+            noweModele_Maszyn.MODEL = this.textBox1.Text;
+            noweModele_Maszyn.MARKA = this.textBox2.Text;
+            noweModele_Maszyn.TYP_MASZYNY = this.textBox5.Text;
+            noweModele_Maszyn.PRĘDKOŚĆ = int.Parse(this.textBox3.Text);
+            noweModele_Maszyn.POSUW = int.Parse(this.textBox6.Text);
+            noweModele_Maszyn.KOSZT_ZA_H = decimal.Parse(this.textBox4.Text);
+
+            db.MODELE_MASZYN.Add(noweModele_Maszyn);
+            db.SaveChanges();
+        }
+
+        private void WyczyscDane_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox3.Text))
+            {
+                int parsedVal;
+                if (!int.TryParse(textBox3.Text, out parsedVal))
+                {
+                    MessageBox.Show("To pole może zawierać tylko liczbę. ");
+                    textBox3.Clear();
+                    return;
+                }
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox6.Text))
+            {
+                int parsedVal;
+                if (!int.TryParse(textBox6.Text, out parsedVal))
+                {
+                    MessageBox.Show("To pole może zawierać tylko liczbę. ");
+                    textBox6.Clear();
+                    return;
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox4.Text))
+            {
+                int parsedVal;
+                if (!int.TryParse(textBox4.Text, out parsedVal))
+                {
+                    MessageBox.Show("To pole może zawierać tylko liczbę. ");
+                    textBox4.Clear();
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ PROJEKTapp/PROJEKTapp/Forms_Produkcja/FormNoweNarzedzie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJEKTapp.Forms_Produkcja
+{
+    public partial class FormNoweNarzedzie : Form
+    {
+        KWZP_PROJEKTEntities db;
+        public FormNoweNarzedzie(KWZP_PROJEKTEntities db)
+           
+        {
+                this.db = db;
+                InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NARZEDZIA noweNarzedzie = new NARZEDZIA();
+            noweNarzedzie.NAZWA = this.textBox1.Text;
+            noweNarzedzie.MODEL = this.textBox2.Text;
+            noweNarzedzie.NR_EWIDENCYJNY = this.textBox3.Text;
+            noweNarzedzie.KOSZT_ZA_H = decimal.Parse(this.textBox4.Text);
+
+
+            db.NARZEDZIA.Add(noweNarzedzie);
+            db.SaveChanges();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox3.Text))
+            {
+                int parsedVal;
+                if (!int.TryParse(textBox3.Text, out parsedVal))
+                {
+                    MessageBox.Show("To pole może zawierać tylko liczbę. ");
+                    textBox3.Clear();
+                    return;
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox4.Text))
+            {
+                int parsedVal;
+                if (!int.TryParse(textBox4.Text, out parsedVal))
+                {
+                    MessageBox.Show("To pole może zawierać tylko liczbę. ");
+                    textBox4.Clear();
+                    return;
+                }
+            }
+        }
+    }
+}
+
